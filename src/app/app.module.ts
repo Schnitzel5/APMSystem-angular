d1a4e734394c5ccaf34e34ee557f187627025a5f
Philip Duong <tangphilip3@gmail.com>
2022-07-07 16:07:37 +0200

    basic number system

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,10 +11,21 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatButtonModule } from '@angular/material/button';
 import { RouterModule, Routes } from '@angular/router';
+import {MatExpansionModule} from "@angular/material/expansion";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import {MatDatepickerModule} from "@angular/material/datepicker";
+import {MatNativeDateModule, MatOptionModule} from "@angular/material/core";
+import {MatAutocompleteModule} from "@angular/material/autocomplete";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {MatMenuModule} from "@angular/material/menu";
+import { NumberSystemComponent } from './number-system/number-system.component';
+import {MatSelectModule} from "@angular/material/select";
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'profile', component: ProfileComponent },
+  { path: 'number-system', component: NumberSystemComponent },
   { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
 
@@ -22,17 +33,29 @@ const routes: Routes = [
   declarations: [
     AppComponent,
     LoginComponent,
-    ProfileComponent
+    ProfileComponent,
+    NumberSystemComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatNativeDateModule,
     MatToolbarModule,
     MatIconModule,
     MatSidenavModule,
     MatButtonModule,
     RouterModule.forRoot(routes),
+    MatExpansionModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatAutocompleteModule,
+    ReactiveFormsModule,
+    MatOptionModule,
+    MatMenuModule,
+    FormsModule,
+    MatSelectModule,
   ],
   exports: [
     RouterModule,
